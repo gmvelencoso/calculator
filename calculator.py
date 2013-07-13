666b8389d09d60c6d47450a063c4967163b65bbf
Gerard Montes <gmvelencoso@gmail.com>
2013-07-13 14:01:55 +0200

    Calculator done, only int, and only sums and rests

--- calculator.py
+++ calculator.py
@@ -2,12 +2,15 @@ def main():
     exp = raw_input('Input:')
     print(operate(exp))
 
-
 def operate(string):
-    try:
-	return eval(string)
-    except:
-	return 'Error'
+    # tests if string is an arithmetic expression and only sums or rests
+    for i in string:
+	if i not in "0123456789+-":
+	    return 'Error'
+
+    return eval(string)
+	"""Ahora solo opera si son enteros y si se suma o se resta, python lo opera bien
+	aunque se le ponga 4--4, que es una operacion correcta."""
 
 if __name__ == '__main__':
-	main()
+    main()
